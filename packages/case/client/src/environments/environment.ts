15d74f2aa80c1e052495f3f4637fa9526b3ff91f
Bruno <bruno@buddyweb.fr>
2023-06-06 17:01:21 +0200

    merge client and server into monorepo

--- packages/case/client/src/environments/environment.ts
+++ packages/case/client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   // TODO: Change this URL once server and client are loaded on the same server.
-  settingsEndpointUrl: 'http://localhost:3000/app-rules/settings'
+  settingsEndpointUrl: '/api/app-rules/settings'
 }
---
+++ packages/case/server/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core'
+import * as express from 'express'
+
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true })
+
+  app.setGlobalPrefix('api')
+  app.use(express.urlencoded({ limit: '50mb', extended: true }))
+
+  // Static files (including client app).
+  app.use(express.static('public'))
+
+  await app.listen(3000)
+}
+bootstrap()
