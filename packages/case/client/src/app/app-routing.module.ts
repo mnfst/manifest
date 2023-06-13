796e54c0e696a580bce07ddf9c210811403dd842
SebConejo <sebastien.conejo@buddyweb.fr>
2023-06-13 15:37:01 +0200

    style header with responsive rules

--- packages/case/client/src/app/app-routing.module.ts
+++ packages/case/client/src/app/app-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
 import { dynamicEntityRoutes } from './dynamic-entity/dynamic-entity.routes'
+import { HomeComponent } from './pages/home/home.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes.concat(dynamicEntityRoutes))],
