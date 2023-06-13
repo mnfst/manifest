b4c4c01e5fc72c7c8cf5f9f800719bb09a03484d
SebConejo <sebastien.conejo@buddyweb.fr>
2023-06-13 15:35:52 +0200

    style: set touch menu HTML structure

--- packages/case/client/src/app/components/touch-menu/touch-menu.component.ts
+++ packages/case/client/src/app/components/touch-menu/touch-menu.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-touch-menu',
   templateUrl: './touch-menu.component.html',
   styleUrls: ['./touch-menu.component.scss']
 })
-export class TouchMenuComponent {
-
-}
+export class TouchMenuComponent {}
