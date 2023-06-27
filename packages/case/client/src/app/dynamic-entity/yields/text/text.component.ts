fb691b53503c60b7860c19dd903213c477f819e3
Bruno <bruno@buddyweb.fr>
2023-06-27 09:11:17 +0200

    feat(projectStructure): fields and yields folders

---
+++ packages/case/client/src/app/dynamic-entity/yields/text/text.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text',
+  templateUrl: './text.component.html',
+  styleUrls: ['./text.component.scss']
+})
+export class TextComponent {
+
+}
