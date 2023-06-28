037918c6c8a38efa9a7564c46ad8670912364a82
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2023-06-28 15:02:12 +0200

    feat: add date input

--- packages/case/client/src/app/dynamic-entity/inputs/date-input/date-input.component.ts
+++ packages/case/client/src/app/dynamic-entity/inputs/date-input/date-input.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild
+} from '@angular/core'
+import { PropertyDescription } from '~shared/interfaces/property-description.interface'
 
 @Component({
   selector: 'app-date-input',
-  templateUrl: './date-input.component.html',
+  template: `<label [for]="prop.propName">{{ prop.label }}</label>
+    <input
+      class="input"
+      type="date"
+      [value]="value ? value : today"
+      (change)="onChange($event)"
+      #input
+    />`,
   styleUrls: ['./date-input.component.scss']
 })
-export class DateInputComponent {
+export class DateInputComponent implements OnInit {
+  @Input() prop: PropertyDescription
+  @Input() value: string
 
+  @Output() valueChanged: EventEmitter<number> = new EventEmitter()
+
+  @ViewChild('input', { static: true }) input: ElementRef
+
+  today = Date.now()
+
+  ngOnInit(): void {
+    if (this.value !== undefined) {
+      this.input.nativeElement.value = this.value
+    }
+  }
+
+  onChange(event: any) {
+    this.valueChanged.emit(event.target.value)
+  }
 }
