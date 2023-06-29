bb84a6b7932d8dcb47f0b6d656c4e9156a44db29
Sébastien Conejo <sebastien.conejo@buddyweb.fr>
2023-06-29 16:43:29 +0200

    style= align currency yield to the right and display it in one line

--- packages/case/client/src/app/dynamic-entity/yields/currency-yield/currency-yield.component.ts
+++ packages/case/client/src/app/dynamic-entity/yields/currency-yield/currency-yield.component.ts
@@ -2,8 +2,8 @@ import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-currency-yield',
-  template: `<span *ngIf="value"> {{ value }} € </span>
-    <span *ngIf="!value"> - </span>`,
+  template: `<span class="is-nowrap" *ngIf="value"> {{ value }} € </span>
+    <span class="is-nowrap" *ngIf="!value"> - </span>`,
   styleUrls: ['./currency-yield.component.scss']
 })
 export class CurrencyYieldComponent {
