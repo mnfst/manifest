4b9aec753bb89e9a36d05117fad5c1731a035a19
Sébastien Conejo <sebastien.conejo@buddyweb.fr>
2023-06-29 16:44:45 +0200

    style: set style of number yield

--- packages/case/client/src/app/dynamic-entity/yields/number-yield/number-yield.component.ts
+++ packages/case/client/src/app/dynamic-entity/yields/number-yield/number-yield.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-number-yield',
-  template: `<span>
+  template: `<span class="is-nowrap">
     {{ value }}
   </span>`,
   styleUrls: ['./number-yield.component.scss']
