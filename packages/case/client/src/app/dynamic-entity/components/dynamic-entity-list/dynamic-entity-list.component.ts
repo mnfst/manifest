e4c9b446a706c8e54a1c85d658700719b1a07609
Bruno <bruno@buddyweb.fr>
2023-07-10 11:27:01 +0200

    feat(UI): flash messages

--- packages/case/client/src/app/dynamic-entity/components/dynamic-entity-list/dynamic-entity-list.component.ts
+++ packages/case/client/src/app/dynamic-entity/components/dynamic-entity-list/dynamic-entity-list.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Params, Router } from '@angular/router'
 import { of } from 'rxjs'
 import { PropType } from '~shared/enums/prop-type.enum'
+import { EntityDescription } from '~shared/interfaces/entity-description.interface'
 import { PropertyDescription } from '~shared/interfaces/property-description.interface'
 
+import { BreadcrumbService } from '../../../services/breadcrumb.service'
+import { FlashMessageService } from '../../../services/flash-message.service'
 import { SettingsService } from '../../../services/settings.service'
 import { DynamicEntityService } from '../../dynamic-entity.service'
-import { BreadcrumbService } from '../../../services/breadcrumb.service'
-import { EntityDescription } from '~shared/interfaces/entity-description.interface'
 
 @Component({
   selector: 'app-dynamic-entity-list',
@@ -29,6 +30,7 @@ export class DynamicEntityListComponent implements OnInit {
     private router: Router,
     private dynamicEntityService: DynamicEntityService,
     private breadcrumbService: BreadcrumbService,
+    private flashMessageService: FlashMessageService,
     settingsService: SettingsService
   ) {
     settingsService
@@ -68,6 +70,9 @@ export class DynamicEntityListComponent implements OnInit {
     this.dynamicEntityService
       .delete(this.entity.definition.slug, id)
       .then((res) => {
+        this.flashMessageService.success(
+          `The ${this.entity.definition.nameSingular} has been deleted.`
+        )
         this.items = this.items.filter((item: any) => item.id !== id)
       })
   }
