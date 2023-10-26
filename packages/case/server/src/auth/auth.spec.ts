7c68440ac0dec36c47e56b2e0f5d28a0eca128f5
= <jshapkin+github@gmail.com>
2023-10-26 10:02:40 -0600

    Add unit test for handling null users
    -Add unit test to check AuthService.getUserFromToken throws an HTTP error when no valid user is found in the repository.

--- packages/case/server/src/auth/auth.spec.ts
+++ packages/case/server/src/auth/auth.spec.ts
@@ -1,61 +1,69 @@
 import { Test } from '@nestjs/testing'
-import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
-import { DataSource } from 'typeorm'
 import { ConfigService } from '@nestjs/config'
+import { DataSource } from 'typeorm'
 import * as jwt from 'jsonwebtoken'
+import { HttpException } from '@nestjs/common'
 
 describe('AuthService', () => {
-    let authService: AuthService;
-    let configService: ConfigService;
+  let authService: AuthService
+  let configService: ConfigService
 
-    const mockDataSource = {
-        getRepository: jest.fn().mockReturnValue({
-          findOne: jest.fn()
-        })
-    }
+  const mockDataSource = {
+    getRepository: jest.fn().mockReturnValue({
+      findOne: jest.fn()
+    })
+  }
 
-    const mockConfigService = {
-        get: jest.fn().mockReturnValue('test-jwt-secret')
-    }
+  const mockConfigService = {
+    get: jest.fn().mockReturnValue('test-jwt-secret')
+  }
 
-    const mockUser = {
-        email: "testEmail",
-        password: "testHashedPassword"
-    }
+  const mockUser = {
+    email: 'testEmail',
+    password: 'testHashedPassword'
+  }
 
-    beforeEach( async () => {
-        const module = await Test.createTestingModule({
-            providers: [
-                AuthService,
-                {
-                    provide: DataSource,
-                    useValue: mockDataSource,
-                },
-                ConfigService, 
-                {
-                    provide: ConfigService,
-                    useValue: mockConfigService,
-                }
-            ]
-        }).compile();
-        
-        authService = module.get<AuthService>(AuthService);
-        configService = module.get<ConfigService>(ConfigService);
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: DataSource,
+          useValue: mockDataSource
+        },
+        ConfigService,
+        {
+          provide: ConfigService,
+          useValue: mockConfigService
+        }
+      ]
+    }).compile()
+
+    authService = module.get<AuthService>(AuthService)
+    configService = module.get<ConfigService>(ConfigService)
+  })
+
+  describe('getUserFromToken', () => {
+    it('should return a valid jwt token if a user is found', async () => {
+      mockDataSource.getRepository().findOne.mockReturnValue(mockUser)
+
+      const result = await authService.createToken(
+        'testEmail',
+        'testPlainPassword'
+      )
+      expect(result).toHaveProperty('token')
+
+      const decodedPayload = jwt.decode(result.token)
+      expect(decodedPayload).toHaveProperty('email', 'testEmail')
     })
 
-    describe('getUserFromToken', () => {
+    it('should throw an exception when no user is found', async () => {
+      mockDataSource.getRepository().findOne.mockReturnValue(null)
 
-        it('should return a valid jwt token if a user is found', async () => {
-            mockDataSource.getRepository().findOne.mockReturnValue(mockUser)
-    
-            const result = await authService.createToken('testEmail', 'testPlainPassword')
-            expect(result).toHaveProperty('token')
-    
-            const decodedPayload: any = jwt.decode(result.token);
-            expect(decodedPayload).toHaveProperty('email', 'testEmail');
-        })
-        
+      expect(async () => {
+        await authService.createToken('testEmail', 'testPlainPassword')
+      }).rejects.toThrow(HttpException)
     })
-    
+  })
 })
