8a6ba9936d260335613b25590333cbfed8342795
Bruno <bruno@buddyweb.fr>
2024-03-01 18:54:17 +0100

    feat: remove schematics

--- packages/schematics/src/commands/create-entity/files/__name@dasherize__.entity.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEntity, Entity, Prop } from '@casejs/case'
-
-@Entity()
-export class <%= classifiedName %> extends BaseEntity {
-  @Prop()
-  name: string
-
-  // Learn more about CASE Entities and how to add properties here: https://docs.case.app/properties
-}
