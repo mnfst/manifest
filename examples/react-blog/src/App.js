6001e622c4d5e50b8f47df3b37f4dfd35f79fd12
Bruno <bruno@buddyweb.fr>
2024-03-04 20:54:38 +0100

    feat(reactBlog): list and detail pages

--- examples/react-blog/src/App.js
+++ examples/react-blog/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import BlogList from './BlogList'
+import BlogPost from './BlogPost'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="p-8">
+        <Routes>
+          <Route exact path="/" element={<BlogList />} />
+          <Route path="/post/:postId" element={<BlogPost />} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
