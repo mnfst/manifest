636f7ed4beefe642bcee5aaf50361f5c823e7799
Bruno <bruno@buddyweb.fr>
2024-03-05 17:44:53 +0100

    feat(validation): TS validation for relationship

--- packages/core/manifest/src/index.ts
+++ packages/core/manifest/src/index.ts
@@ -10,7 +10,6 @@ import manifestSchema from './json-schema/schema/manifest-schema.json'
 const appFile = fs.readFileSync(`${process.cwd()}/manifest/backend.yml`, 'utf8')
 const appManifest: any = yaml.load(appFile)
 
-// TODO: Validation against JSON schema.
 let validate: any = new Ajv({
   schemas: [manifestSchema, entitySchema, propertySchema, relationshipSchema]
 }) // options can be passed, e.g. {allErrors: true}
@@ -19,12 +18,28 @@ validate = validate.getSchema(manifestSchema.$id)
 const valid = validate(appManifest)
 
 if (!valid) {
-  console.error('Validation failed ####')
+  console.error('JSON Schema Validation failed ####')
   console.error(validate.errors)
+  process.exit(1)
 } else {
-  console.log('## Validation passed !')
+  console.log('## JSON Schema Validation passed !')
 }
 
-// TODO: Extra validation steps
+console.log('## App Manifest:', appManifest)
+
+// 1. Extra validation Check if the entity in relationship exists.
+const entities = Object.keys(appManifest.entities)
+
+Object.values(appManifest.entities).forEach((entity: any) => {
+  const relationships = Object.values(entity.hasMany || {})
+    .concat(Object.values(entity.belongsTo || {}))
+    .map((relationship: any) => relationship.entity)
+
+  relationships.forEach((relationship: any) => {
+    if (!entities.includes(relationship)) {
+      console.error(`Entity ${relationship} does not exist in the manifest`)
+    }
+  })
+})
 
 // TODO: Connect with CASE.
