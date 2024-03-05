dc1648c17cd6bad2e6e322ef2668363561213814
Bruno <bruno@buddyweb.fr>
2024-03-05 19:07:22 +0100

    feat(core): TypeORM and sqlite

--- packages/core/manifest/src/app.module.ts
+++ packages/core/manifest/src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+
+import { TypeOrmModule } from '@nestjs/typeorm'
+import configuration from './configuration'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>
+        configService.get('database'),
+      inject: [ConfigService]
+    })
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
--- packages/core/manifest/src/main.ts
+++ packages/core/manifest/src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import * as express from 'express'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true
+  })
+
+  const configService = app.get(ConfigService)
+
+  app.setGlobalPrefix('api')
+  app.use(express.urlencoded({ limit: '50mb', extended: true }))
+  app.useGlobalPipes(new ValidationPipe())
+
+  await app.listen(configService.get('port'))
 }
-bootstrap();
+bootstrap()
