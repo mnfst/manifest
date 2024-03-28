b5d2488a9517041a2b9e6c3dbd2478a48c60ea96
Bruno <bruno@buddyweb.fr>
2024-03-28 16:47:40 +0100

    feat(jsonSchema): specific property options

--- packages/core/manifest/src/manifest/json-schema/index.ts
+++ packages/core/manifest/src/manifest/json-schema/index.ts
@@ -1,4 +1,7 @@
 import entitySchema from './definitions/entity-schema.json'
+import propertyOptionsSchema from './definitions/property-options-schema.json'
+import choiceOptionsSchema from './definitions/property-options/choice-options-schema.json'
+import moneyOptionsSchema from './definitions/property-options/money-options-schema.json'
 import propertySchema from './definitions/property-schema.json'
 import relationshipSchema from './definitions/relationship-schema.json'
 import manifestSchema from './manifest-schema.json'
@@ -7,5 +10,8 @@ export default [
   manifestSchema,
   entitySchema,
   propertySchema,
-  relationshipSchema
+  relationshipSchema,
+  propertyOptionsSchema,
+  choiceOptionsSchema,
+  moneyOptionsSchema
 ]
