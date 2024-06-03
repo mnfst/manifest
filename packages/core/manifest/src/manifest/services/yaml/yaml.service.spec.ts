972fb8865f2714abfb5ef3a720d3fd9910f1fccb
Bruno <bruno@buddyweb.fr>
2024-06-03 17:33:14 +0200

    clean yaml service

--- packages/core/manifest/src/manifest/services/yaml/yaml.service.spec.ts
+++ packages/core/manifest/src/manifest/services/yaml/yaml.service.spec.ts
@@ -1,21 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { YamlService } from './yaml.service'
-import { ConfigService } from '@nestjs/config'
 
 describe('YamlService', () => {
   let service: YamlService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        YamlService,
-        {
-          provide: ConfigService,
-          useValue: {
-            get: jest.fn()
-          }
-        }
-      ]
+      providers: [YamlService]
     }).compile()
 
     service = module.get<YamlService>(YamlService)
@@ -23,5 +14,11 @@ describe('YamlService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('should load the manifest from the YAML file and transform it into a AppManifest object', () => {
+    const manifest = service.load()
+
+    expect(manifest).toBeDefined()
   })
 })
