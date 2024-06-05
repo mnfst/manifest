f7ccb227e0580b0f5f5f7c593ccbc2645b0939fd
Bruno <bruno@buddyweb.fr>
2024-06-05 12:02:33 +0200

    test: e2e health

---
+++ packages/core/manifest/e2e/tests/health.e2e-spec.ts
@@ -0,0 +1,39 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import supertest from 'supertest'
+
+import TestAgent from 'supertest/lib/agent'
+import { AppModule } from '../../src/app.module'
+
+describe('Health (e2e)', () => {
+  let originalConsoleLog
+
+  beforeAll(() => {
+    originalConsoleLog = console.log
+    console.log = jest.fn()
+  })
+
+  afterAll(() => {
+    console.log = originalConsoleLog
+  })
+
+  let app: INestApplication
+  let request: TestAgent
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
+
+    app = moduleFixture.createNestApplication()
+    request = supertest(app.getHttpServer())
+    await app.init()
+  })
+
+  it('/GET health', () => {
+    return request
+      .get('/health')
+      .expect(200)
+      .expect(JSON.stringify({ status: 'OK' }))
+  })
+})
