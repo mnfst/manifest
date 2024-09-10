7a13b98f4dfd2aba78d77deb5dc4b26a40ed367d
Bruno <bruno@buddyweb.fr>
2024-09-10 15:23:40 +0200

    feat(manifest): allow emoji after entity name

--- packages/core/manifest/src/manifest/services/yaml.service.ts
+++ packages/core/manifest/src/manifest/services/yaml.service.ts
@@ -23,34 +23,32 @@ export class YamlService {
       'utf8'
     )
 
-    const Manifest: Manifest = yaml.load(fileContent) as Manifest
+    const manifest: Manifest = yaml.load(fileContent) as Manifest
 
     // Remove emojis from entity keys.
-    Object.keys(Manifest.entities).forEach((key) => {
+    Object.keys(manifest.entities).forEach((key) => {
       const newKey: string = this.ignoreEmojis(key)
       if (newKey !== key) {
-        Manifest.entities[newKey] = Manifest.entities[key]
-        delete Manifest.entities[key]
+        manifest.entities[newKey] = manifest.entities[key]
+        delete manifest.entities[key]
       }
     })
 
-    return Manifest
+    return manifest
   }
 
   /**
    *
-   * Ignore emojis from the file content. Also remove the optional space after the emoji if it exists.
+   * Ignore emojis from the file content. Also remove the optional space before or after the emoji if it exists.
    *
    * @param fileContent
    * @returns string the file content without emojis
    *
    */
   ignoreEmojis(fileContent: string): string {
-    // Regex to match emojis and any space immediately following the emoji
+    const emojiNearDirectSpaceRegex =
+      /([\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{2E00}-\u{2E7F}\u{3000}-\u{303F}\u{FE00}-\u{FE0F}]\s?)/gu
 
-    const emojiAndDirectSpaceRegex =
-      /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{2E00}-\u{2E7F}\u{3000}-\u{303F}\u{FE00}-\u{FE0F}]\s?/gu
-
-    return (fileContent || '').replace(emojiAndDirectSpaceRegex, '')
+    return (fileContent || '').replace(emojiNearDirectSpaceRegex, '').trim()
   }
 }
