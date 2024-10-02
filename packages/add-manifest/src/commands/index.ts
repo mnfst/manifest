0a82bc3a2727235a31c14ec5dbd64dc58f7385cc
Bruno <bruno@buddyweb.fr>
2024-10-02 09:38:03 +0200

    update version number

--- packages/add-manifest/src/commands/index.ts
+++ packages/add-manifest/src/commands/index.ts
@@ -91,7 +91,7 @@ export class MyCommand extends Command {
       updatePackageJsonFile({
         fileContent: packageJson,
         newPackages: {
-          manifest: '^4.0.0-beta'
+          manifest: '^4.0.0'
         },
         newScripts: {
           manifest: 'node node_modules/manifest/scripts/watch/watch.js',
