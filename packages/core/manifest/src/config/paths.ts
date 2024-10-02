d4e00021efd720edf7e6b2db1b591933c5032af0
Bruno <bruno@buddyweb.fr>
2024-10-02 10:02:14 +0200

    feat(pack): cp admin into manifest dist

--- packages/core/manifest/src/config/paths.ts
+++ packages/core/manifest/src/config/paths.ts
@@ -1,9 +1,14 @@
+import path from 'path'
+
 export default (): {
   paths: { adminPanelFolder: string; manifestFile: string }
 } => {
   return {
     paths: {
-      adminPanelFolder: `${process.cwd()}/node_modules/@mnfst/admin/dist`,
+      adminPanelFolder:
+        process.env.NODE_ENV === 'contribution'
+          ? path.join(process.cwd(), '..', 'admin', 'dist')
+          : `${process.cwd()}/node_modules/manifest/dist/admin`,
       manifestFile: `${process.cwd()}/manifest/backend.yml`
     }
   }
