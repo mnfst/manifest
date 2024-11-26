1220e374a4ba47700c6eaa5743706bf6da87d0da
Bruno <bruno@buddyweb.fr>
2024-11-26 11:34:33 +0100

    feat(manifest): single type entity manifest

---
+++ packages/core/manifest/src/manifest/utils/policy-manifests.ts
@@ -0,0 +1,5 @@
+import { PolicyManifest } from '@repo/types'
+
+export const adminAccessPolicy: PolicyManifest = { access: 'admin' }
+export const publicAccessPolicy: PolicyManifest = { access: 'public' }
+export const forbiddenAccessPolicy: PolicyManifest = { access: 'forbidden' }
