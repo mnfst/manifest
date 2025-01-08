9c71db9f0a0e0ed2774caf97b42ad97551364755
Bruno <bruno@buddyweb.fr>
2025-01-08 21:19:10 +0100

    tests: pagination and some custom validators

--- packages/core/manifest/src/crud/services/pagination.service.ts
+++ packages/core/manifest/src/crud/services/pagination.service.ts
@@ -10,8 +10,6 @@ export class PaginationService {
    * @param query The query to paginate.
    * @param resultsPerPage The number of results to return per page.
    * @param currentPage The current page.
-   * @param transformResult A function to transform each result.
-   * @param asyncTransformResult An async function to transform each result.
    *
    * @returns A promise that resolves to a Paginator object.
    *
@@ -19,36 +17,19 @@ export class PaginationService {
   async paginate({
     query,
     resultsPerPage,
-    currentPage,
-    transformResult,
-    asyncTransformResult
+    currentPage
   }: {
     query?: SelectQueryBuilder<BaseEntity>
     currentPage: number
     resultsPerPage?: number
-    transformResult?: (result: BaseEntity) => BaseEntity
-    asyncTransformResult?: (result: BaseEntity) => Promise<BaseEntity>
   }): Promise<Paginator<BaseEntity>> {
     const offset: number = (currentPage - 1) * resultsPerPage
-    let total: number
-    let results: BaseEntity[]
 
-    total = await query.getCount()
-    results = await query.skip(offset).take(resultsPerPage).getMany()
-
-    // Apply individual function transformResult() on each result if provided.
-    if (transformResult) {
-      results.forEach((result: any) => {
-        result = transformResult(result)
-      })
-    }
-    if (asyncTransformResult) {
-      results = await Promise.all(
-        results.map((result) =>
-          asyncTransformResult(result).then((res: any) => res)
-        )
-      )
-    }
+    const total: number = await query.getCount()
+    const results: BaseEntity[] = await query
+      .skip(offset)
+      .take(resultsPerPage)
+      .getMany()
 
     return {
       data: results,
---
+++ packages/core/manifest/src/validation/tests/custom-validators.spec.ts
@@ -0,0 +1,191 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { PropType } from '@repo/types'
+import { ValidationService } from '../services/validation.service'
+
+describe('Custom validators', () => {
+  let service: ValidationService
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ValidationService]
+    }).compile()
+
+    service = module.get<ValidationService>(ValidationService)
+  })
+
+  it('min type expects a value greater than or equal to the min value', async () => {
+    const goodValues = [3, 4, 5]
+    const badValues = [1, 2]
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.Number,
+        validation: { min: 3 }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.Number,
+        validation: { min: 3 }
+      })
+    )
+
+    console.log(goodValidations, badValidations)
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('max type expects a value less than or equal to the max value', async () => {
+    const goodValues = [1, 2, 3]
+    const badValues = [4, 5]
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.Number,
+        validation: { max: 3 }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.Number,
+        validation: { max: 3 }
+      })
+    )
+
+    console.log(goodValidations, badValidations)
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('isEmpty type expects a value to be empty', async () => {
+    const goodValues = [undefined, null, '']
+    const badValues = [1, 'test']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isEmpty: true }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isEmpty: true }
+      })
+    )
+
+    console.log(goodValidations, badValidations)
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('isNotEmpty type expects a value to not be empty', async () => {
+    const goodValues = ['1', 'test']
+    const badValues = [undefined, null, '']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isNotEmpty: true }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isNotEmpty: true }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('required type expect a value to not be empty (same as isNotEmpty)', async () => {
+    const goodValues = ['1', 'test']
+    const badValues = [undefined, null]
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { required: true }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { required: true }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('isDefined type expects a value to be defined', async () => {
+    const goodValues = ['1', 'test']
+    const badValues = [undefined, null]
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isDefined: true }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isDefined: true }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  // TODO: Continue with the rest of the custom validators.
+})
