5e05a99a4ea4ff934834c7b8558fadcb2b6d6ebb
Bruno <bruno@buddyweb.fr>
2025-02-12 11:22:59 +0100

    feat(postgres): SSL env var

--- packages/core/manifest/src/config/database.ts
+++ packages/core/manifest/src/config/database.ts
@@ -24,9 +24,7 @@ export default (): {
         password: process.env.DB_PASSWORD || 'postgres',
         database: process.env.DB_DATABASE || 'manifest',
         dropSchema: process.env.DB_DROP_SCHEMA === 'true' || false,
-        ssl: {
-          rejectUnauthorized: false
-        },
+        ssl: process.env.DB_SSL === 'true' || false,
         synchronize: true
       }
     }
