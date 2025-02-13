a7e238295d6ac6f1810029bafb7e4d0a282adde0
Bruno <bruno@buddyweb.fr>
2025-02-13 16:02:22 +0100

    tests: missing tests

--- packages/core/manifest/src/entity/tests/entity.service.spec.ts
+++ packages/core/manifest/src/entity/tests/entity.service.spec.ts
@@ -6,6 +6,14 @@ import { DataSource } from 'typeorm'
 describe('EntityService', () => {
   let service: EntityService
   let dataSource: DataSource
+  let entityManifestService: EntityManifestService
+
+  const entityMetadatas = [
+    {
+      targetName: 'Entity',
+      columns: []
+    }
+  ]
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,7 +28,7 @@ describe('EntityService', () => {
         {
           provide: DataSource,
           useValue: {
-            entityMetadatas: [],
+            entityMetadatas,
             getRepository: jest.fn()
           }
         }
@@ -29,25 +37,78 @@ describe('EntityService', () => {
 
     service = module.get<EntityService>(EntityService)
     dataSource = module.get<DataSource>(DataSource)
+    entityManifestService = module.get<EntityManifestService>(
+      EntityManifestService
+    )
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
   })
 
-  describe('getEntityRepository', () => {
-    it('should fail if no entity metadata or entity slug is provided', () => {
-      expect(() => {
-        service.getEntityRepository({})
-      }).toThrow()
+  describe('getEntityMetadatas', () => {
+    it('should get entity metadatas in the correct order', () => {
+      jest.spyOn(service, 'sortEntitiesByHierarchy')
+
+      service.getEntityMetadatas()
+
+      expect(service.sortEntitiesByHierarchy).toHaveBeenCalledWith(
+        entityMetadatas
+      )
+    })
+  })
+
+  describe('getEntityMetadata', () => {
+    it('should fail if no className or slug is provided', () => {
+      expect(() => service.getEntityMetadata({})).toThrowError()
     })
 
+    it('should get entity metadata by className', () => {
+      const result = service.getEntityMetadata({
+        className: 'Entity'
+      })
+
+      expect(entityManifestService.getEntityManifest).not.toHaveBeenCalled()
+      expect(result.targetName).toBe('Entity')
+    })
+
+    it('should get entity metadata by slug', () => {
+      jest.spyOn(entityManifestService, 'getEntityManifest').mockReturnValue({
+        className: 'Entity'
+      } as any)
+
+      const result = service.getEntityMetadata({
+        slug: 'entity'
+      })
+
+      expect(entityManifestService.getEntityManifest).toHaveBeenCalledWith({
+        slug: 'entity',
+        fullVersion: true
+      })
+
+      expect(result.targetName).toBe('Entity')
+    })
+
+    it('should fail if no entity metadata is found', () => {
+      jest.spyOn(entityManifestService, 'getEntityManifest').mockReturnValue({
+        className: 'AnotherEntity'
+      } as any)
+
+      expect(() =>
+        service.getEntityMetadata({
+          slug: 'entity'
+        })
+      ).toThrow()
+    })
+  })
+
+  describe('getEntityRepository', () => {
     it('should return a repository', () => {
       const entityMetadata = {
         target: 'Entity'
       } as any
 
-      const result = service.getEntityRepository({
+      service.getEntityRepository({
         entityMetadata
       })
 
--- packages/core/manifest/src/validation/tests/custom-validators.spec.ts
+++ packages/core/manifest/src/validation/tests/custom-validators.spec.ts
@@ -181,5 +181,353 @@ describe('Custom validators', () => {
     )
   })
 
-  // TODO: Continue with the rest of the custom validators.
+  // TODO: equals and not equals.
+
+  it('isIn type expects a value to be in the list of values', async () => {
+    const goodValues = ['1', 'test']
+    const badValues = ['bad', 'value']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isIn: ['1', 'test'] }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isIn: ['1', 'test'] }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('isNotIn type expects a value to not be in the list of values', async () => {
+    const goodValues = ['bad', 'value']
+    const badValues = ['1', 'test']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isNotIn: ['1', 'test'] }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isNotIn: ['1', 'test'] }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('contains type expects a value to contain the seed', async () => {
+    const goodValues = ['test', 'testing']
+    const badValues = ['bad', 'value']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { contains: 'test' }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { contains: 'test' }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('notContains type expects a value to not contain the seed', async () => {
+    const goodValues = ['bad', 'value']
+    const badValues = ['test', 'testing']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { notContains: 'test' }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { notContains: 'test' }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('isAlpha type expects a value to contain only letters', async () => {
+    const goodValues = ['test', 'TestinG']
+    const badValues = ['bad1', 'value2']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isAlpha: true }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isAlpha: true }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('isAlphanumeric type expects a value to contain only letters and numbers', async () => {
+    const goodValues = ['test1', 'TestinG2']
+    const badValues = ['bad1!', 'value2@']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isAlphanumeric: true }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isAlphanumeric: true }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('isAscii type expects a value to contain only ASCII characters', async () => {
+    const goodValues = ['test', 'TestinG']
+    const badValues = ['bad1€', 'value2✗']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isAscii: true }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isAscii: true }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('isEmail type expects a value to be a valid email address', async () => {
+    const goodValues = ['test@test.com', 'example@test.io']
+    const badValues = ['bad', 'value']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isEmail: true }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isEmail: true }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('isJSON type expects a value to be a valid JSON string', async () => {
+    const goodValues = ['{"test": "value"}', '{"test": 1}']
+    const badValues = ['bad', 'value']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isJSON: true }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { isJSON: true }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('minLength type expects a value to be at least the min length', async () => {
+    const goodValues = ['test', 'testing']
+    const badValues = ['bad', 'value']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { minLength: 4 }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { minLength: 6 }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('maxLength type expects a value to be at most the max length', async () => {
+    const goodValues = ['test', 'testing']
+    const badValues = ['bad', 'value']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { maxLength: 7 }
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { maxLength: 2 }
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('matches type expects a value to match the pattern', async () => {
+    const goodValues = ['test', 'testing']
+    const badValues = ['notG@@d', 'n€ither']
+
+    const goodValidations = goodValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { matches: /^[a-z]+$/ } as any
+      })
+    )
+
+    const badValidations = badValues.map((value) =>
+      service.validateProperty(value, {
+        name: 'test',
+        type: PropType.String,
+        validation: { matches: /^[a-z]+$/ } as any
+      })
+    )
+
+    expect(goodValidations.every((validation) => validation.length === 0)).toBe(
+      true
+    )
+    expect(badValidations.every((validation) => validation.length === 1)).toBe(
+      true
+    )
+  })
+
+  it('isOptional type always returns null', async () => {
+    const value = 'test'
+
+    const validation = service.validateProperty(value, {
+      name: 'test',
+      type: PropType.String,
+      validation: { isOptional: true }
+    })
+
+    expect(validation.length).toBe(0)
+  })
 })
