36ae77e6faf005560e1ec6d669b4345207439b54
Bruno <bruno@buddyweb.fr>
2025-02-24 11:13:30 +0100

    fix(tests): handler test failing

--- packages/core/manifest/src/handler/handler.service.spec.ts
+++ packages/core/manifest/src/handler/handler.service.spec.ts
@@ -88,7 +88,7 @@ describe('HandlerService', () => {
       )
 
       await expect(service.importHandler('handler')).rejects.toThrow(
-        'Handler is invalid'
+        'Handler not found'
       )
     })
 
@@ -100,6 +100,7 @@ describe('HandlerService', () => {
           default: handler
         })
       )
+      ;(fs.existsSync as jest.Mock).mockReturnValue(true)
 
       const result = await service.importHandler('handler')
 
