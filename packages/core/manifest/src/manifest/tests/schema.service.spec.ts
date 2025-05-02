79ad6f146966a100d1d370e50df866ab87d39479
Bruno <bruno@buddyweb.fr>
2025-05-02 13:34:53 +0200

    tests: schema service

--- packages/core/manifest/src/manifest/tests/schema.service.spec.ts
+++ packages/core/manifest/src/manifest/tests/schema.service.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { SchemaService } from '../services/schema.service'
+import schemas from '@repo/json-schema'
+import { Manifest } from '../../../../types/src'
 
 describe('SchemaService', () => {
   let service: SchemaService
 
-  const manifest: any = {
+  const manifest: Manifest = {
     name: 'test app',
-    entities: {}
+    entities: {
+      Cat: {
+        className: 'Cat'
+      }
+    }
   }
 
   beforeEach(async () => {
@@ -23,5 +29,50 @@ describe('SchemaService', () => {
 
   it('should validate a manifest', () => {
     expect(service.validate(manifest)).toBe(true)
+  })
+
+  describe('Validate against schema', () => {
+    it('should console log errors when schema validation fails', () => {
+      jest.spyOn(console, 'log').mockImplementation(() => {})
+      jest.spyOn(process, 'exit').mockImplementation(() => {
+        return null as never
+      })
+
+      service.validateAgainstSchema('wrong manifest' as any, schemas[0])
+
+      expect(console.log).toHaveBeenCalledWith(
+        expect.stringContaining('Validation failed')
+      )
+    })
+  })
+
+  describe('Validate custom logic', () => {
+    it('should console log errors when relationship entities do not exist', () => {
+      const manifestWithNonExistingRelationships: Manifest = {
+        name: 'test app',
+        entities: {
+          Cat: {
+            className: 'Cat',
+            belongsTo: [
+              {
+                name: 'Dragon',
+                entity: 'Dragon'
+              }
+            ]
+          }
+        }
+      }
+
+      jest.spyOn(console, 'log').mockImplementation(() => {})
+      jest.spyOn(process, 'exit').mockImplementation(() => {
+        return null as never
+      })
+
+      service.validateCustomLogic(manifestWithNonExistingRelationships)
+
+      expect(console.log).toHaveBeenCalledWith(
+        expect.stringContaining('Validation failed')
+      )
+    })
   })
 })
