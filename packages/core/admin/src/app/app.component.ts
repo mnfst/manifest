258f0fd9c6738a1e9792a393807613535b5df8c0
Bruno Perez <bruno@buddyweb.fr>
2025-09-11 15:04:41 +0200

    fix(admin): white screen on reload on deep pages

--- packages/core/admin/src/app/app.component.ts
+++ packages/core/admin/src/app/app.component.ts
@@ -26,20 +26,20 @@ export class AppComponent implements OnInit, OnDestroy {
   ) {}
 
   async ngOnInit() {
+    this.currentUserSubscription = this.authService.currentUser$.subscribe(
+      (admin) => {
+        if (!admin && localStorage.getItem(TOKEN_KEY)) {
+          this.authService.loadCurrentUser().subscribe()
+        }
+      }
+    )
+
     this.router.events.subscribe((routeChanged) => {
       if (routeChanged instanceof NavigationEnd) {
         window.scrollTo(0, 0)
         this.isLogin =
           routeChanged.url.includes('/auth/login') ||
           routeChanged.url.includes('/auth/welcome')
-
-        this.currentUserSubscription = this.authService.currentUser$.subscribe(
-          (admin) => {
-            if (!admin && localStorage.getItem(TOKEN_KEY)) {
-              this.authService.loadCurrentUser().subscribe()
-            }
-          }
-        )
       }
     })
 
