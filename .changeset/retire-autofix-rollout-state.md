e4b6e6ce06d587ef2fd1a9cd81ee4c8598745a34
Guillaume Gay <guillaume.gay@protonmail.com>
2026-07-31 16:13:20 +0200

    chore: remove legacy Auto-fix rollout state (#2670)

---
+++ .changeset/retire-autofix-rollout-state.md
@@ -0,0 +1,5 @@
+---
+'manifest': patch
+---
+
+Remove the legacy Auto-fix tenant rollout columns after general availability.
--- packages/backend/src/database/data-source-definitions.ts
+++ packages/backend/src/database/data-source-definitions.ts
@@ -37,6 +37,7 @@ import { AddTenantRequestUsage1801300000000 } from './migrations/1801300000000-A
 import { AddRequestRecordings1801300000000 } from './migrations/1801300000000-AddRequestRecordings';
 import { MoveRecordingsToProviderAttempts1801400000000 } from './migrations/1801400000000-MoveRecordingsToProviderAttempts';
 import { EnableRecordingForNewAgents1801500000000 } from './migrations/1801500000000-EnableRecordingForNewAgents';
+import { DropLegacyAutofixRolloutColumns1801600000000 } from './migrations/1801600000000-DropLegacyAutofixRolloutColumns';
 import { InitialSchema1771464895790 } from './migrations/1771464895790-InitialSchema';
 import { HashApiKeys1771500000000 } from './migrations/1771500000000-HashApiKeys';
 import { ModelPricingImprovements1771600000000 } from './migrations/1771600000000-ModelPricingImprovements';
@@ -312,4 +313,5 @@ export const migrations = [
   AddRequestRecordings1801300000000,
   MoveRecordingsToProviderAttempts1801400000000,
   EnableRecordingForNewAgents1801500000000,
+  DropLegacyAutofixRolloutColumns1801600000000,
 ];
